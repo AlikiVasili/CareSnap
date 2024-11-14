9eae5c4aff044e45ea30418610cebf73eb172c37
Aliki Vasili <avasil08@ucy.ac.cy>
2024-11-14 14:08:52 +0200

    fix UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 import { HeaderComponent } from './header/header.component';
 import { AboutSectionComponent } from "./about-section/about-section.component";
-import { MENU_ITEMS_LIST } from './menu_items_list';
-import { MenuItemComponent } from "./menu-item/menu-item.component";
 import { PatientSummarySectionComponent } from "./patient-summary-section/patient-summary-section.component";
 import { NcpehSectionComponent } from "./ncpeh-section/ncpeh-section.component";
 import { TestUserSectionComponent } from "./test-user-section/test-user-section.component";
 import { ChatbotComponent } from "./chatbot/chatbot.component";
-
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, AboutSectionComponent, MenuItemComponent, PatientSummarySectionComponent, NcpehSectionComponent, TestUserSectionComponent, ChatbotComponent],
+  imports: [
+    CommonModule,  // Add CommonModule here
+    HeaderComponent,
+    AboutSectionComponent,
+    PatientSummarySectionComponent,
+    NcpehSectionComponent,
+    TestUserSectionComponent,
+    ChatbotComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'CareSnap-project';
-  menuItems = MENU_ITEMS_LIST;
   selectedMenuItemId = "i1";
+  activeMenuItem: any;
 
-  get selectedMenuItem(){
-    return this.menuItems.find((menuItem) => menuItem.id === this.selectedMenuItemId);
-  }
-
-  onSelectedMenuItem(id: string){
+  onSelectedMenuItem(id: string) {
     this.selectedMenuItemId = id;
   }
-
 }
